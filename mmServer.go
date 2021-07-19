b98375f727b8dd0004fd6036b62af802d5b723c6
ashutosh.j <ashutosh.j@media.net>
2021-07-19 19:20:31 +0530

    seprate mm server

---
+++ mmServer.go
@@ -0,0 +1,47 @@
+package matchmaker
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+type CreateWebsocketConnectionRequest struct {
+	Name string
+}
+
+type MatchmakingServer struct {
+	TicketChan     chan *Player
+	ConnectionsMap map[string]*websocket.Conn
+	Connections    map[*websocket.Conn]bool
+	Upgrader       websocket.Upgrader
+}
+
+func (server MatchmakingServer) handlePlayerTickets(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		var req RequestTickets
+		decoder := json.NewDecoder(r.Body)
+		err := decoder.Decode(&req)
+		if err != nil {
+			fmt.Println("Error in handling enqueue player ", err)
+			return
+		}
+		player := req.Json2Player()
+		server.addPlayerTicket(&player)
+	}
+}
+
+func (server MatchmakingServer) addPlayerTicket(p *Player) {
+	server.TicketChan <- p
+}
+
+func (server MatchmakingServer) Start() {
+	http.HandleFunc("/search", server.handlePlayerTickets)
+	fmt.Printf("Matchmaking server started...\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
--- tickets.go
+++ tickets.go
@@ -1,13 +1,7 @@
 package matchmaker
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
 	"time"
-
-	"github.com/gorilla/websocket"
 )
 
 type RequestTickets struct {
@@ -17,43 +11,6 @@ type RequestTickets struct {
 	ttl   int64
 }
 
-type CreateWebsocketConnectionRequest struct {
-	Name string
-}
-
-type MatchmakingServer struct {
-	TicketChan     chan *Player
-	ConnectionsMap map[string]*websocket.Conn
-	Connections    map[*websocket.Conn]bool
-	Upgrader       websocket.Upgrader
-}
-
 func (r RequestTickets) Json2Player() Player {
 	return NewPlayer(r.Name, r.Skill, r.Ping, time.Now().Unix()+r.ttl)
-}
-
-func (server MatchmakingServer) handlePlayerTickets(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "POST" {
-		var req RequestTickets
-		decoder := json.NewDecoder(r.Body)
-		err := decoder.Decode(&req)
-		if err != nil {
-			fmt.Println("Error in handling enqueue player ", err)
-			return
-		}
-		player := req.Json2Player()
-		server.addPlayerTicket(&player)
-	}
-}
-
-func (server MatchmakingServer) addPlayerTicket(p *Player) {
-	server.TicketChan <- p
-}
-
-func (server MatchmakingServer) Start() {
-	http.HandleFunc("/search", server.handlePlayerTickets)
-	fmt.Printf("Matchmaking server started...\n")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal(err)
-	}
 }
